97dc8e0b4a8581a307bbdd6bc4a8a7c83fd87dc8
heh <heh>
2023-02-14 22:04:09 +0000

    spotify add and fonts

--- download_packages.sh
+++ download_packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacaur -S polybar brave-bin neovim picom zsh kitty bitwarden ttf-font-awesome conky-lua-nv tere feh optimus-manager zsh-syntax-highlighting-git --noconfirm
+pacaur -S polybar brave-bin neovim picom zsh kitty bitwarden ttf-font-awesome conky-lua-nv tere feh optimus-manager zsh-syntax-highlighting-git terminus-font ttf-font-awesome libcurl-gnutls spotify playerctl zscroll-git --noconfirm

b92b5034f80c2cd39801bef9c3baa6a63b86c64c
heh <heh>
2023-02-15 18:07:41 +0000

    update

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ~/.config/i3/config ~/autorice/i3/config
+cp ~/.config/kitty/kitty.conf ~/autorice/kitty/kitty.conf
+cp ~/.config/polybar/config.ini ~/autorice/polybar/config.ini
+cp -r ~/.config/polybar/scripts ~/autorice/polybar/

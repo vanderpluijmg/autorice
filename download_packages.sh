be5ab6ee1c30aac2789c56c437c67a23600c3559
mynameisjonatron <mynameisjonatron>
2023-02-22 15:23:41 +0000

    update

--- download_packages.sh
+++ download_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | gpg --import -
 
-pacaur -S polybar brave-bin neovim picom zsh kitty bitwarden ttf-font-awesome conky-lua-nv tere feh optimus-manager zsh-syntax-highlighting-git terminus-font ttf-font-awesome libcurl-gnutls spotify playerctl zscroll-git rofi pulseaudio oh-my-zsh-git --noconfirm
+pacaur -S polybar brave-bin neovim picom zsh kitty bitwarden ttf-font-awesome conky-lua-nv tere feh optimus-manager zsh-syntax-highlighting-git terminus-font ttf-font-awesome libcurl-gnutls spotify playerctl zscroll-git rofi pulseaudio oh-my-zsh-git signal-desktop obsidian filelight --noconfirm
 
 pulseaudio --start
